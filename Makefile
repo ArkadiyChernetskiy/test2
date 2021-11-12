4d2a27727c93e8f91a4992a8d657e395b2098846
ArkadiyChernetskiy <arkadiy.chernetskiy@phystech.edu>
2021-11-13 01:31:03 +0300

    fix3

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ build:
 	g++ -std=c++17 hello.cpp
 
 test1:
-	./.github/res_test.sh
+	./tests/res_test.sh
 clean:
 	rm a.out
 	rm out.txt
--- tests/res_test.sh
+++ tests/res_test.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-./.github/generate_arrays.py > test.txt
+./tests/generate_arrays.py > test.txt
 A=$(head -n 1 test.txt)
 echo $A
 tail -n +2 test.txt > test_without_sizes.txt
-./.github/generate_answer.py > answer.txt
+./tests/generate_answer.py > answer.txt
 ./a.out $A < test_without_sizes.txt > out.txt
-./.github/check_answer.py
+./tests/check_answer.py
