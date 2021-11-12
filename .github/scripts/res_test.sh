5be37301c3657bec51943a15cd90077259dc9f21
ArkadiyChernetskiy <arkadiy.chernetskiy@phystech.edu>
2021-11-13 01:48:25 +0300

    fix8

--- .github/scripts/res_test.sh
+++ .github/scripts/res_test.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-python ./tests/generate_arrays.py > test.txt
+script_path=./.github/scripts
+
+python $script_path/generate_arrays.py > test.txt
 A=$(head -n 1 test.txt)
-echo $A
 tail -n +2 test.txt > test_without_sizes.txt
-python ./tests/generate_answer.py > answer.txt
+python $script_path/generate_answer.py > answer.txt
 ./a.out $A < test_without_sizes.txt > out.txt
-python ./tests/check_answer.py
+python $script_path/check_answer.py
