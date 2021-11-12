401bbbd7200af136affa40313b64c191f2bc30f3
ArkadiyChernetskiy <arkadiy.chernetskiy@phystech.edu>
2021-11-13 01:27:00 +0300

    fix

--- .github/res_test.sh
+++ .github/res_test.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-./.github/workflows/generate_arrays.py > test.txt
+./.github/generate_arrays.py > test.txt
 A=$(head -n 1 test.txt)
 echo $A
 tail -n +2 test.txt > test_without_sizes.txt
-./.github/workflows/generate_answer.py > answer.txt
+./.github/generate_answer.py > answer.txt
 ./a.out $A < test_without_sizes.txt > out.txt
-./.github/workflows/check_answer.py
+./.github/check_answer.py
