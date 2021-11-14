83533cb4af517e27d907a8d0a48e9280f80165ae
ArkadiyChernetskiy <arkadiy.chernetskiy@phystech.edu>
2021-11-14 20:07:04 +0300

    fix sizes

--- .github/scripts/generate_arrays.py
+++ .github/scripts/generate_arrays.py
@@ -3,10 +3,10 @@
 import random
 
 result = []
-k = random.randint(2,8)
+k = random.randint(2,6)
 for i in range (0,k):
     result.append ([])
-    for j in range (0, random.randint(5,10)):
+    for j in range (0, random.randint(5,8)):
         result[-1].append (random.randint(1,100))
 
 print (' '.join ([str(len(i)) for i in result]))
