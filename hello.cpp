c6ffc2d93bd1bc26ace431720d13838878f70607
ArkadiyChernetskiy <arkadiy.chernetskiy@phystech.edu>
2021-11-15 18:26:22 +0300

    ff

--- hello.cpp
+++ hello.cpp
@@ -55,6 +55,7 @@ int main (int argc, char** argv)//массив строк= массив масс
         }
     }
     std::cout<< answer;
+    std::cout.flush();
     //осталось правильно прописать все delete
     delete[] ind;
     for (int i=0; i<k; i++){
