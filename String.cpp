3a92ba37c83884455fddcd4a953309661ab33b6d
ArkadiyChernetskiy <arkadiy.chernetskiy@phystech.edu>
2021-11-15 18:57:31 +0300

    test

--- String.cpp
+++ String.cpp
@@ -1,250 +1,167 @@
 //
-// Created by YUIP on 04.11.2021.
+// Created by Olga Akimova on 02.11.2021.
 //
 
-#ifndef STRING_STRING_H
-#define STRING_STRING_H
+#ifndef INC_1_1_STRING_STRING_H
+#define INC_1_1_STRING_STRING_H
 #include <iostream>
 #include <cstring>
 
-//
-class String {
+class String{
 private:
-    char *data ;//array of chars
-    size_t size ;//current_length
-    size_t capacity ;//max_length
-    void changeCapacity(size_t newSize);
-    void expandCapacity ();
-
+    size_t size, capacity;
+    char* data;
+    void changeCapacity(size_t newCapacity) {
+        char* newData = new char[newCapacity];
+        memcpy(newData, data, size);
+        delete[] data;
+        data = newData;
+        capacity = newCapacity;
+    }
+    size_t findInSuffix(const String& substr, size_t left_border) const {
+        for (size_t left = left_border; left <= size - substr.size; left++) {
+            for (size_t right = left; right < size; right++) {
+                if (data[right] != substr.data[right - left])
+                    break;
+                if (right - left + 1 == substr.size)
+                    return left;
+            }
+        }
+        return size;
+    }
 public:
-    String ( const char* c);
-    String( size_t n, char c);
-    String( const String& str); //конструктор копирования
-    String& operator=( const String& other) &;
-    String(); // конструктор по умолчанию
-    ~String();
-    friend bool operator==(const String& first, const String& second);
-    friend bool operator==(const String& s, const char& c);
-    friend bool operator==(const char& c, const String& s);
-
-    void push_back (char a);
-    void pop_back();
-    char front() const;
-    char back() const;
-    char& front();
-    char& back();
-    size_t length() const;
-
-    char& operator[] (size_t index) ;
-    char operator[] (size_t index) const;
-    String& operator += (const String& other);
-    String& operator+=( const char& c);
-
-    size_t find( const String& other) const;
-    size_t rfind( const String& other) const;
-    String substr( size_t start, size_t count) const;
-
-    friend std::ostream& operator <<(std::ostream& out, const String& s);
-    friend std::istream& operator >>(std::istream& in, String& s) ;
-
-    bool empty() const;
-    void clear();
-
-
-};
-//private functions
-void String::changeCapacity(size_t newSize){
-    capacity = newSize;
-    char* newData = new char[newSize];
-    //char* nn = new char[2];
-    memcpy (newData, data, size);
-    delete[] (data);
-    data = newData;
-}
-void String::expandCapacity (){
-    if (size >= capacity){
-        changeCapacity (size*2);
+    String(const char* str): size(strlen(str)), capacity(size), data(new char[capacity]) {
+        memcpy(data, str, size);
     }
-}
-//constructors and so on
-String::String (const char* c){
-    size=strlen(c);
-    capacity = strlen(c)*2+1;
-    data = new char[capacity];
-    memcpy (data, c, size);
-}
-
-String::String() {
-    data= new char [1];
-    size=0;
-    capacity=1;
-}
-
-String::String( size_t n, char c){
-    size=n;
-    capacity=2*n+1;
-    data=new char[capacity];
-    // memset (data, c, n);
-    for (size_t i=0; i<size; i++){
-        data[i]=c;
+    String(size_t len, char symbol): size(len), capacity(len), data(new char[capacity]) {
+        memset(data, symbol, size);
     }
-}
-String::String( const String& str) : data(new char[str.capacity]), size(str.size), capacity(str.capacity){
-    memcpy( data, str.data, size);
-}
-
-bool operator==(const String& first, const String& second){
-    if ( first.size!=second.size) return false;
-    if ( memcmp(first.data, second.data, first.size)==0){ return true;}
-    return false;
-}
-
-bool operator==(const String& s, const char& c){
-    if (s.size!=1) return false;
-    if (s[0]!=c) return false;
-    return true;
-};
-
-bool operator==(const char& c, const String& s){
-    return ( s==c);
-};
-String& String::operator=( const String& other) &{
-    if (this == &other) {
+    String(): size(0), capacity(8), data(new char[capacity]) {}
+    String(const String& str): size(str.size), capacity(str.size), data(new char[size]) {
+        memcpy(data, str.data, size);
+    }
+    ~String() {
+        delete[] data;
+    }
+    void swap(String& str) {
+        std::swap(size, str.size);
+        std::swap(capacity, str.capacity);
+        std::swap(data, str.data);
+    }
+    String& operator=(const String& str) {
+        String str_copy = str;
+        swap(str_copy);
         return *this;
     }
-    size = other.size;
-    capacity=other.capacity;
-    delete[] data;
-    data = new char[capacity];
-    memcpy(data, other.data, size);
-    return *this; // адрес- this
-}
-String::~String() {
-    delete[] data;//array of chars
-}
-
-void String::push_back (char a){
-    expandCapacity();
-    size+=1;
-    data[size-1]=a;
-}
-void String::pop_back(){
-    size-=1;
-}
-
-char String::front() const{
-    return data[0];
-}
-char& String::front() {
-    return data[0];
-}
-
-char String::back() const{
-    return data[size-1];
-}
-char& String::back(){
-    return data[size-1];
-}
-
-
-char String::operator[] (size_t index) const{
-    return data[index];
-}
-char& String::operator[] (size_t index) {
-    return data[index];
-}
-
-String& String::operator+= (const String& other){ //работает только для константной строки(прибавляемой) или для всех?
-    if ( capacity< size+other.size){
-        changeCapacity(2*(size+other.size));
+    char& operator[](size_t index) {
+        return data[index];
     }
-    memcpy(&data[size], other.data,  other.size); //data
-    size+=other.size;
-    return *this;
-}
-String& String::operator+=( const char& c){
-    push_back(c);
-    return *this;
-}
-String operator+( const String& first,const String& second){
-    String new_str=first;
-    new_str+=second;
-    return new_str;
-}
-String operator+( const String& str, const char& c){
-    return str+String(1, c);//мб стоит здесь поменять
-}
-String operator+( const char& c, const String& str) {
-    return String(1, c)+str;
-}
-
-size_t String::find( const String& other) const{
-    if (other.size==0){
-        return 0;
+    char operator[](size_t index) const {
+        return data[index];
     }
-    if (other.size>size){
-        return length();
+    size_t length() const {
+        return size;
     }
-    size_t i=0;
-    while( i<size-other.size){
-        if (substr(i, other.size)==other) return i;
-        i++;
+    void push_back(char symbol) {
+        if (size == capacity)
+            changeCapacity(capacity * 2);
+        data[size++] = symbol;
     }
-    return length();
+    char pop_back() {
+        char last_element = data[--size];
+        if (size * 4 <= capacity && capacity >= 16)
+            changeCapacity(capacity / 2);
+        return last_element;
+    }
+    char& front() {
+        return data[0];
+    }
+    char front() const {
+        return data[0];
+    }
+    char& back() {
+        return data[size - 1];
+    }
+    char back() const {
+        return data[size - 1];
+    }
+    String& operator+=(char symbol) {
+        push_back(symbol);
+        return *this;
+    }
+    String& operator+=(const String& str) {
+        if (capacity < size + str.size)
+            changeCapacity(size + str.size);
+        memcpy(data + size, str.data, str.size);
+        size += str.size;
+        return *this;
+    }
+    size_t find(const String& substr) const {
+        return findInSuffix(substr, 0);
+    }
+    size_t rfind(const String& substr) const {
+        size_t next = findInSuffix(substr, 0), last = next;
+        while (next != size) {
+            last = next;
+            next = findInSuffix(substr, last + 1);
+        }
+        return last;
+    }
+    String substr(size_t left, size_t count) const {
+        String sub_str;
+        if (sub_str.capacity < count)
+            sub_str.changeCapacity(count);
+        memcpy(sub_str.data, data + left, count);
+        sub_str.size = count;
+        return sub_str;
+    }
+    bool empty() const {
+        return size == 0;
+    }
+    void clear() {
+        delete[] data;
+        size = 0;
+        capacity = 8;
+        data = new char[capacity];
+    }
+    bool operator==(const String& str) const {
+        if (size != str.size)
+            return false;
+        return (memcmp(data, str.data, size) == 0);
+    }
 };
-size_t String::rfind( const String& other) const{
-    if (other.size==0){
-        return length()-1;
-    }
-    if (other.size>size){
-        return length();
-    }
-    size_t i=size-other.size+1; //индекс с которого начинаем читать строку
-    while (i>0){
-        if (substr(i-1, other.size)==other) return i-1;
-        i--;
-    }
-    return length();
+
+String operator+(const String& first, const String& second) {
+    String sum(first);
+    sum += second;
+    return sum;
+}
+String operator+(const String& first, char second) {
+    String sum(first);
+    sum += second;
+    return sum;
+}
+String operator+(char first, const String& second) {
+    String sum(1, first);
+    sum += second;
+    return sum;
 }
 
-String String::substr( size_t start, size_t count) const{
-    String sub( count, '\0');
-    memcpy(sub.data, data+start, count);
-    return sub;
-}
-
-std::ostream& operator <<(std::ostream& out, const String& s) {
-    for (size_t i=0; i<s.size; i++){
-        out<< s.data[i];
-    }
-    return out;
-};
-std::istream& operator >>(std::istream& in, String& s) {
-    s.clear();
+std::istream& operator>>(std::istream &in, String& str) {
+    str.clear();
     char c;
-    while (isspace(in.peek())){
-        if(!in.get(c)) { break; }
-    }
-
-    while ( in.get(c) && !isspace(c) ){
-        s.push_back(c);
-    }
+    do {
+        in.get(c);
+    } while(isspace(c));
+    do {
+        str.push_back(c);
+    } while(in.get(c) && !isspace(c));
     return in;
-};
-
-size_t String::length() const{
-    return size;
-}
-bool String::empty() const{
-    if (size==0){
-        return true;
-    }
-    return false;
-}
-void String::clear(){
-    size=0;
-    changeCapacity(1);
-
 }
 
-#endif //STRING_STRING_H
+std::ostream& operator<<(std::ostream &out, const String& str) {
+    for (size_t i = 0; i < str.length(); i++)
+        out << str[i];
+    return out;
+}
+#endif //INC_1_1_STRING_STRING_H
