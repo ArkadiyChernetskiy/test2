1b7d954ab6d70b338b1e69876779a1d698627358
ArkadiyChernetskiy <arkadiy.chernetskiy@phystech.edu>
2022-06-20 12:21:38 +0300

    Variant little test

--- .github/cpp_tests/work_test.cpp
+++ .github/cpp_tests/work_test.cpp
@@ -1,13 +1,7 @@
-#include "Shared_test.cpp"
-#include "Yandex_test.cpp"
-#include "Code_style_test.cpp"
-#include "Weak_test.cpp"
+#include "Yandex_tests.cpp"
 
 
 int main()
 {
-    yandex_test();
-    shared_test();
-    weak_test();
-    code_test();
+    yandex_tests();
 }
